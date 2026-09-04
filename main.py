68216a993fff93c3cee797d3d12fcb9f072bcdd4
Taleb88 <tdamaree18@gmail.com>
2026-09-03 22:13:02 -0400

    various updates - continued analysis

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc84fd11c607ccb71c5bf9897c05d9e9273f906d0d7976e03689ca0d3628ce4b
-size 2847
+oid sha256:3d95401d6523fc911c277a30a48fe3fb968b0a3dc6e00c63f3b310c37b2dfaef
+size 3298
