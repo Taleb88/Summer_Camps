ce3bff3ffa98d443d72870be1afb531e217300f6
Taleb88 <tdamaree18@gmail.com>
2026-09-04 17:04:59 -0400

    continued analysis via power bi

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b3ba99db3e6df4eb9256c9c922b942129fea5a9058cfcc05bfb1980964c4237
-size 3357
+oid sha256:8ad422723d29f37ba932790b3628683d9c04a47d67d543b5813703c48f9ec186
+size 3417
