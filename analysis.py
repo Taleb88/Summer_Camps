6f93845ae66eb48f4976f0d975bb6e6585019fa8
Taleb88 <tdamaree18@gmail.com>
2026-09-05 23:08:25 -0400

    continued analysis

--- analysis.py
+++ analysis.py
@@ -12,3 +12,5 @@ print('\n west coast states df:\n', west_coast_states_df)
 
 print(new_york_state_df[new_york_state_df['review_score'] <= 4.0])
 print("\n camps in east coast states with scholarships",east_coast_states_df[(east_coast_states_df['scholarships_available'] == 'Yes') & (east_coast_states_df['category'] == 'Sports')])
+
+#xlookup type
