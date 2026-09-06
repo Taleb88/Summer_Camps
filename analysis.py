c8f23d89fb57cf57142a286eeee755e3c5e36be2
Taleb88 <tdamaree18@gmail.com>
2026-09-06 12:28:33 -0400

    continued analysis

--- analysis.py
+++ analysis.py
@@ -13,7 +13,28 @@ print('\n west coast states df:\n', west_coast_states_df)
 print(new_york_state_df[new_york_state_df['review_score'] <= 4.0])
 print("\n camps in east coast states with scholarships",east_coast_states_df[(east_coast_states_df['scholarships_available'] == 'Yes') & (east_coast_states_df['category'] == 'Sports')])
 
-# calculations
-midwest_states_df['avg_age'] = (midwest_states_df['min_age'].astype(float) + midwest_states_df['max_age'].astype(float)) / 2
+# exploratory analysis (calculations, etc.....)
+def avg_age(midwest_states_df):
+    try:
+        return (midwest_states_df['min_age'].astype(float) + midwest_states_df['max_age'].astype(float)) / 2
+    except Exception as e:
+        print(f'error - cannot create new column or perform calculations at this time - {type(e)}')
 
-print(midwest_states_df[['camp_name','avg_age']])
+midwest_states_df['avg_age'] = avg_age(midwest_states_df)
+print(midwest_states_df[['camp_name','avg_age', 'city', 'state_abbr']].head(20))
+
+
+def rating(west_coast_states_df):
+    try:
+        if 
+
+        elif
+
+        elif
+
+        elif
+    except Exception as e:
+        print(f'error - unable to determine pass/fail score per review_score {type(e)}')
+
+midwest_states_df['recommended?'] = rating(west_coast_states_df)
+print(midwest_states_df[['camp_name','avg_age', 'city', 'state_abbr','recommended?']].head(20))
